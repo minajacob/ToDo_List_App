d95b8bd748f29079b281704b7b0145d2fd498c64
Mina Jacob <mina.j.adly@gmail.com>
2021-03-19 22:14:40 +0200

    refs Menu Design

--- src/app/modules/controls/controls.module.ts
+++ src/app/modules/controls/controls.module.ts
@@ -4,12 +4,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
 
 const components = [
   MatSidenavModule,
   MatToolbarModule,
   MatIconModule,
-  MatListModule
+  MatListModule,
+  MatRippleModule
 ]
 
 @NgModule({
